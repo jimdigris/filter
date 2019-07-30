5db48fb61f2eb618a6bdf0d9f97975fe0e59c7c1
jimdigris <polyakov.alekseyka@gmail.com>
2019-07-30 23:01:58 +0300

    начало

--- filter.js
+++ filter.js
@@ -4,6 +4,32 @@
 'use strict';
 
 (function () {
+	
+	let base = [
+		{
+			massa: 10,
+			width: 11,
+			height: 12
+		},
+		{
+			massa: 20,
+			width: 21,
+			height: 22
+		},		
+		{
+			massa: 30,
+			width: 31,
+			height: 32
+		},
+		{
+			massa: 40,
+			width: 41,
+			height: 42
+		}		
+	]
+	
+	const filtersWrap = document.querySelector('.filter_wrap');
 
+	console.log(base[3].width);
 
 })();
