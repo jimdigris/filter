f3adbfc2b5086dbb7e4b411a49eca5dd00720ef8
jimdigris <polyakov.alekseyka@gmail.com>
2019-07-31 23:44:10 +0300

    в работе

--- filter.js
+++ filter.js
@@ -7,32 +7,49 @@
 	
 	let base = [
 		{
-			massa: 10,
-			width: 11,
-			height: 12
+			title: 'Товар №1',
+			img: 'http://zaokurs.ru/assets/images/products/253/medium/maket-dlya-miniatyur-2019.jpg',
+			content: 'контент 1',
+			massa: 10
 		},
 		{
-			massa: 20,
-			width: 21,
-			height: 22
+			title: 'Товар №2',
+			img: 'http://zaokurs.ru/assets/images/products/254/medium/maket-dlya-miniatyur-20191.jpg',
+			content: 'контент 2',
+			massa: 20
 		},		
 		{
-			massa: 30,
-			width: 31,
-			height: 32
+			title: 'Товар №3',
+			img: 'http://zaokurs.ru/assets/images/products/255/medium/maket-dlya-miniatyur-2019.jpg',
+			content: 'контент 3',
+			massa: 30
 		},
 		{
-			massa: 40,
-			width: 41,
-			height: 42
+			title: 'Товар №4',
+			img: 'http://zaokurs.ru/assets/images/products/260/medium/akkumulyator-fl-500mp-y5.jpg',
+			content: 'контент 4',
+			massa: 40
+		},
+		{
+			title: 'Товар №5',
+			img: 'http://zaokurs.ru/assets/images/products/253/medium/maket-dlya-miniatyur-2019.jpg',
+			content: 'контент 5',
+			massa: 50
 		}		
 	]
 	
 	const filtersWrap = document.querySelector('.filter_wrap');
-	const templItemCard = filtersWrap.querySelector('#templItemCard');
+	const templItemCard = filtersWrap.querySelector('#templItemCard').content.querySelector('.itemCard');
 	
+	drawItemsCards ();
 	function drawItemsCards () {
-		
+		for (let i = 0; i < base.length; i++) {
+			let card = templItemCard.cloneNode(true);
+			card.querySelector('.itemTitle').textContent = base[i].title;
+			card.querySelector('.itemImage').src = base[i].img;
+			card.querySelector('.itemContent').textContent = base[i].content;
+			filtersWrap.appendChild(card);
+		}
 	}
 
 })();
